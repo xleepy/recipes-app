a7e1b0ca6c8ba1889255077bce2796ad1194b7f6
Radmir Gurkov <ragux0@gmail.com>
2023-01-10 01:13:10 +0100

    Check env variable in ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
   css: {
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase',
     },
   },
   resolve: {
     alias: {
-      react: "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat", // Must be below test-utils
-      "react/jsx-runtime": "preact/jsx-runtime",
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat', // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime',
     },
   },
 });
