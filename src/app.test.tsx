61c606f2df7240f3c26a83aaf737fce60cda3bdf
Radmir Gurkov <ragux0@gmail.com>
2024-01-16 23:18:06 +0100

    more snapshots

---
+++ src/app.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/preact';
+import { describe, expect, it, vi } from 'vitest';
+import { App } from './app';
+
+describe('App tests', () => {
+  it('should render app', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
